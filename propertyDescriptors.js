2c41bf46742484bbd993b50c0adb2942d8db441e
Ashutosh Kumar <urr.ashu@gmail.com>
2024-10-22 20:57:25 +0530

    property descriptions

--- propertyDescriptors.js
+++ propertyDescriptors.js
@@ -1,39 +1,15 @@
 const obj = { name: "Roy", age: 20 };
-
 // console.log(Object.getOwnPropertyDescriptors(obj));
+//configurable
+Object.defineProperty(obj, "name", {
+  configurable: false,
+});
+obj.name = "Jack";
+delete obj.name;
+obj.age = 30;
+obj.gender = "Female";
+console.log(obj); //Jack
 
-//enumerable - loop
 Object.defineProperty(obj, "name", {
   enumerable: false,
 });
-
-for (const key in obj) {
-  console.log(key);
-}
-console.log(Object.keys(obj));
-
-// //writable
-// Object.defineProperty(obj, "name", {
-//   enumerable: true,
-//   writable: false,
-// });
-// obj.name = "Jack";
-// console.log(obj.name); //Roy
-// for (const key in obj) {
-//   console.log(key);
-// }
-// //configurable
-// Object.defineProperty(obj, "name", {
-//   enumerable: true,
-//   writable: false,
-//   configurable: false,
-// });
-
-// delete obj.name;
-// obj.age = 30;
-// obj.gender = "Female";
-// console.log(obj); //Roy
-
-// Object.defineProperty(obj, "name", {
-//   enumerable: false,
-// });
